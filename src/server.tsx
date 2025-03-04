53d7f2b60b1b3dff8d1b694bf74368a3081076ac
Dilraj <djrandhawa15@hotmail.com>
2025-03-04 14:35:17 -0800

    doing database stuff

--- src/server.tsx
+++ src/server.tsx
@@ -4,6 +4,11 @@ import { IController } from "./shared/interfaces";
 import { prettyJSON } from "hono/pretty-json";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { logger } from "hono/logger";
+import { 
+  Session,
+  sessionMiddleware, 
+  CookieStore 
+} from 'hono-sessions'
 
 export class App {
   private _app: Hono;
@@ -21,6 +26,16 @@ export class App {
   private async initMiddleware() {
     this._app.use("*", logger(), prettyJSON());
     this._app.use("/static/*", serveStatic({ root: "./" }));
+    this._app.use('*', sessionMiddleware({
+      store,
+      encryptionKey: 'password_at_least_32_characters_long', // Required for CookieStore, recommended for others
+      expireAfterSeconds: 900, // Expire session after 15 minutes of inactivity
+      cookieOptions: {
+        sameSite: 'Lax', // Recommended for basic CSRF protection in modern browsers
+        path: '/', // Required for this library to work properly
+        httpOnly: true, // Recommended to avoid XSS attacks
+      },
+    }))
   }
 
   private initErrorHandling() {
